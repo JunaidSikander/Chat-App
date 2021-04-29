7bb22590e4e894d3837f167571d847b28fd9c2e0
Junaid Sikander Hakro <junaid_sikander@live.com>
2021-04-29 15:48:21 +0500

    [ADD] user utils functions

---
+++ backend/utils/users.js
@@ -0,0 +1,28 @@
+ const users = [];
+
+const addUser = ({ id, name, room }) => {
+    name= name.trim().toLocaleLowerCase();
+    room= room.trim().toLocaleLowerCase();
+
+    const existingUsers = users.find((user) => user.room === room && user.name === name);
+
+     if(existingUsers){ return { error: 'Username is taken' } }
+
+     const user = { id, name, room };
+
+     users.push(user);
+
+     return { user };
+};
+
+const removeUser = (id) => {
+    const index = users.find((user) => user.id === id);
+
+    if(index !== -1){ return users.splice(index, 1)[0] }
+};
+
+const getUser = (id) => users.find((user) => user.id === id);
+
+const getUsersInRoom = (room) => users.filter((user) => user.room === room);
+
+export { addUser, removeUser, getUser, getUsersInRoom };
