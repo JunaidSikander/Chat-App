a128eb5cc589c746b7f0765c649560590c01a68d
Junaid Sikander Hakro <junaid_sikander@live.com>
2021-04-29 15:48:00 +0500

    [ADD] send & receive message event

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,54 @@
 import express from 'express';
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+import { addUser, removeUser, getUser, getUsersInRoom } from './utils/users.js'
 
 const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {});
 const port = process.env.PORT || 3131;
 
+//Initialize Socket Connection
+io.on('connection', (socket => {
+    console.log('Connection Established');
 
+    //Receive an event
+    socket.on('join', ({ name, room }, callback) => {
+        //Adding User
+        const { error, user } = addUser({id: socket.id, name, room})
+
+        //Return Callback with an error if occurs
+        if(error) return callback(error);
+
+        //Welcome new enrolled user
+        socket.emit('message',
+            {user: 'admin', text: `${user.name}, welcome to room ${user.room}`});
+
+        //Send Message Trigger to every one except new user who had joined
+         socket.broadcast.to(user.room).emit('message',
+             {user: 'admin', text: `${user.name} has joined!`});
+
+        //User Joining Room
+        socket.join(user.room);
+        
+        callback();
+    });
+
+    // Listen sentMessage Event from frontend
+    socket.on('sentMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        //send event message to a particular room
+        io.to(user.room).emit('message', { user: user.name, text: message })
+
+        callback();
+    });
+
+    // When User Disconnect from Socket
+    socket.on('disconnect', () => {
+        console.log('User had left!!!');
+    });
+}))
 app.get('/', (req, res) => res.send('Server Chat Api'))
 
-app.listen(port, () => console.log(`Server is up on PORT ${port}`))
+httpServer .listen(port, () => console.log(`Server is up on PORT ${port}`))
