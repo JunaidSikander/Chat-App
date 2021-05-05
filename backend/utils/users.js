6e048bb367cb7edbe05d08802391522286f1feb0
Junaid Sikander Hakro <junaid_sikander@live.com>
2021-05-05 11:41:14 +0500

    [FIX] remove function

--- backend/utils/users.js
+++ backend/utils/users.js
@@ -1,28 +1,30 @@
- const users = [];
+const users = [];
 
-const addUser = ({ id, name, room }) => {
-    name= name.trim().toLocaleLowerCase();
-    room= room.trim().toLocaleLowerCase();
+const addUser = ({id, name, room}) => {
+    name = name.trim().toLocaleLowerCase();
+    room = room.trim().toLocaleLowerCase();
 
     const existingUsers = users.find((user) => user.room === room && user.name === name);
 
-     if(existingUsers){ return { error: 'Username is taken' } }
+    if (!name || !room) return {error: 'Username and room are required.'};
+    if (existingUsers) {
+        return {error: 'Username is taken'}
+    }
 
-     const user = { id, name, room };
+    const user = {id, name, room};
 
-     users.push(user);
+    users.push(user);
 
-     return { user };
+    return {user};
 };
 
 const removeUser = (id) => {
-    const index = users.find((user) => user.id === id);
-
-    if(index !== -1){ return users.splice(index, 1)[0] }
+    const index = users.findIndex((user) => user.id === id);
+    if (index !== -1) return users.splice(index, 1)[0];
 };
 
 const getUser = (id) => users.find((user) => user.id === id);
 
 const getUsersInRoom = (room) => users.filter((user) => user.room === room);
 
-export { addUser, removeUser, getUser, getUsersInRoom };
+export {addUser, removeUser, getUser, getUsersInRoom};
